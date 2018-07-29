ed0bc730e783ec044ecdec97279d0413f29f6bdc
John-Sekora <sekora.github@gmail.com>
2018-07-29 11:39:01 -0700

    Initial commit.

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+
+import requests
+from flask import Flask, send_file, Response
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+
+def get_fact():
+
+    response = requests.get("http://unkno.com")
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    facts = soup.find_all("div", id="content")
+
+    return facts[0].getText()
+
+
+@app.route('/')
+def home():
+
+    fact = get_fact()
+    r = requests.post("http://hidden-journey-62459.herokuapp.com/piglatinize/",
+                      allow_redirects=False,
+                      data={'input_text': fact})
+
+    stencil = """<a href={}>{}</a>"""
+    display = stencil.format(r.headers['location'], r.headers['location'])
+
+    return display
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 6787))
+    app.run(host='0.0.0.0', port=port)
